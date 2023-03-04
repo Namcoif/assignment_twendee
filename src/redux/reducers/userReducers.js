a31147485f133311b968a7180cd513026042dd00
Nguyễn Trường Nam <Namcoif@gmail.com>
2023-03-04 21:33:18 +0700

    complete

---
+++ src/redux/reducers/userReducers.js
@@ -0,0 +1,32 @@
+import actionTypes from "../constant/constant";
+
+const initialState = {
+    listUsers: [],
+    statusGet: 'request'
+}
+
+const userReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_LIST_USER_FAIL:
+            return {
+                ...state,
+                statusGet: 'Get list fail!'
+            }
+        case actionTypes.GET_LIST_USER_REQUEST:
+            return {
+                ...state,
+                statusGet: 'Sending request!'
+            }
+        case actionTypes.GET_LIST_USER_SUCCESS:
+            return {
+                ...state,
+                listUsers: action.payload,
+                statusGet: 'Get list successfully!'
+            }
+
+        default:
+            return state
+    }
+}
+
+export default userReducers;
